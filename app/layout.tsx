cc024c6347330cc1261dadde376f05c4f1579715
Argoty <leonardoargotydev@gmail.com>
2024-03-13 12:59:59 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins'
+});
+
+export const metadata: Metadata = {
+  title: "RendezVibe",
+  description: "RendezVibe is a platform for event managmement",
+  icons: {
+    icon: "/assets/images.logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+
+  );
+}
---
+++ components/shared/Header.tsx
@@ -0,0 +1,42 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
+
+function Header() {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image src="/assets/images/logo.svg" width={128} height={38} alt="RendezVibe logo" />
+        </Link>
+
+        <SignedIn>
+          <div className="hidden md:flex-between w-full max-w-xs">
+            <NavItems/>
+          </div>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/"/>
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
+
+      </div>
+
+    </header>
+  )
+}
+
+export default Header
