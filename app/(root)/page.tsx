34314218002444ea79728ac359be7555e3d645b8
Argoty <leonardoargotydev@gmail.com>
2024-03-26 11:44:39 -0500

    implement stripe

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/event.actions";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: '',
+    category: '',
+    page: 1,
+    limit: 6
+  })
+
+  
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -25,6 +35,16 @@ export default function Home() {
           Search
           Category Filter
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
